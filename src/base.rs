d750cb735b9bc2f0b4d9b086e9c2edf2a6f7d77e
Alex Angelini <alex.louis.angelini@gmail.com>
2020-01-07 19:35:10 +0100

    Filter wip

---
+++ src/base.rs
@@ -0,0 +1,62 @@
+use std::cmp;
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct FileId(pub usize);
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct FilterId(pub usize);
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct TagId(pub usize);
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub enum Id {
+    File(FileId),
+    Filter(FilterId),
+    Tag(TagId),
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub enum Comparator {
+    Equal,
+    NotEqual,
+    GreaterThan,
+    GreaterThanEqual,
+    LessThan,
+    LessThanEqual,
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+/// A Closed-Open Interval
+pub struct Interval(pub usize, pub usize);
+
+impl Interval {
+    pub fn is_empty(&self) -> bool {
+        self.0 == self.1
+    }
+
+    pub fn contains(&self, other: Interval) -> bool {
+        other.is_empty()
+            || (self.missing_before(other).is_empty() && self.missing_after(other).is_empty())
+    }
+
+    pub fn missing_before(&self, other: Interval) -> Interval {
+        if other.0 < self.0 {
+            Interval(other.0, self.0)
+        } else {
+            Interval(self.0, self.0)
+        }
+    }
+
+    pub fn missing_after(&self, other: Interval) -> Interval {
+        if other.1 > self.1 {
+            Interval(self.1, other.1)
+        } else {
+            Interval(self.1, self.1)
+        }
+    }
+}
+
+pub trait Bounded {
+    fn bounds(&self) -> Interval;
+}
--- src/repl.rs
+++ src/repl.rs
@@ -4,7 +4,8 @@ use std::path::PathBuf;
 use nom::error::convert_error;
 use rustyline::error::ReadlineError;
 
-use crate::engine::{Command, Engine, Id, Output};
+use crate::base::{Comparator, Id};
+use crate::engine::{Command, Engine, Output};
 use crate::error::{Error, Result, SyntaxError};
 use crate::parser::{self, Expression};
 
@@ -12,58 +13,121 @@ use crate::parser::{self, Expression};
 pub enum Application {
     Load(String, String),
 
-    Regex(String, String),
-    RegexPipe(String),
-
     Tag(String, String),
-    TagPipe(String),
+    TagPiped(String),
+
+    Regex(String, String),
+    RegexPiped(String),
+
+    Transform(String, String, Option<String>),
+    TransformPiped(String, Option<String>),
+
+    DirectFilter(String, Comparator, String),
+    DirectFilterNamed(String, String, Comparator, String),
+    DirectFilterPiped(Comparator, String),
+    DirectFilterPipedNamed(String, Comparator, String),
+
+    ScriptedFilter(String, String, Option<String>),
+    ScriptedFilterNamed(String, String, String, Option<String>),
+    ScriptedFilterPiped(String, Option<String>),
+    ScriptedFilterPipedNamed(String, String, Option<String>),
 
     Take(String, usize),
     TakePipe(usize),
-
-    Transform(String, String, Option<String>),
-    TransformPipe(String, Option<String>),
 }
 
 impl Application {
-    fn from_expression(exp: &Expression) -> std::result::Result<Application, SyntaxError> {
+    #[rustfmt::skip]
+    fn from_expression(
+        exp: &Expression,
+        is_pipelined: bool,
+    ) -> std::result::Result<Application, SyntaxError> {
         if let Expression::Application(func, args) = exp {
             match (func.as_str(), args.as_slice()) {
-                ("load", [Expression::Symbol(file), Expression::String(path)]) => {
+                ("load",
+                 [Expression::Symbol(file), Expression::String(path)]) => {
                     Ok(Application::Load(file.clone(), path.clone()))
                 }
 
-                ("regex", [Expression::Symbol(tag), Expression::String(path)]) => {
-                    Ok(Application::Regex(tag.clone(), path.clone()))
-                }
-                ("regex", [Expression::String(path)]) => Ok(Application::RegexPipe(path.clone())),
-
-                ("tag", [Expression::Symbol(file), Expression::Symbol(tag)]) => {
+                ("tag",
+                 [Expression::Symbol(file), Expression::Symbol(tag)]) => {
                     Ok(Application::Tag(file.clone(), tag.clone()))
                 }
-                ("tag", [Expression::Symbol(tag)]) => Ok(Application::TagPipe(tag.clone())),
+                ("tag",
+                 [Expression::Symbol(tag)]) => {
+                    Ok(Application::TagPiped(tag.clone()))
+                }
+
+                ("regex",
+                 [Expression::Symbol(tag), Expression::String(path)]) => {
+                    Ok(Application::Regex(tag.clone(), path.clone()))
+                }
+                ("regex",
+                 [Expression::String(path)]) => {
+                    Ok(Application::RegexPiped(path.clone()))
+                }
+
+                ("transform",
+                 [Expression::Symbol(tag), Expression::String(transform), Expression::String(setup)]) => {
+                    Ok(Application::Transform(tag.clone(), transform.clone(), Some(setup.clone())))
+                }
+                ("transform",
+                 [Expression::Symbol(tag), Expression::String(transform)]) => {
+                    Ok(Application::Transform(tag.clone(), transform.clone(), None))
+                }
+                ("transform",
+                 [Expression::String(transform), Expression::String(setup)]) => Ok(
+                    Application::TransformPiped(transform.clone(), Some(setup.clone())),
+                ),
+                ("transform",
+                 [Expression::String(transform)]) => {
+                    Ok(Application::TransformPiped(transform.clone(), None))
+                }
+
+                ("filter",
+                 [Expression::Symbol(tag_or_name), Expression::Comparator(comp), Expression::String(value)]) => {
+                    if is_pipelined {
+                        Ok(Application::DirectFilterPipedNamed(tag_or_name.clone(), *comp, value.clone()))
+                    } else {
+                        Ok(Application::DirectFilter(tag_or_name.clone(), *comp, value.clone()))
+                    }
+                }
+                ("filter",
+                 [Expression::Symbol(tag), Expression::Symbol(name), Expression::Comparator(comp), Expression::String(value)]) => {
+                    Ok(Application::DirectFilterNamed(tag.clone(), name.clone(), *comp, value.clone()))
+                }
+                ("filter",
+                 [Expression::Comparator(comp), Expression::String(value)]) => {
+                    Ok(Application::DirectFilterPiped(*comp, value.clone()))
+                }
+                ("filter",
+                 [Expression::Symbol(tag), Expression::String(test)]) => {
+                    Ok(Application::ScriptedFilter(tag.clone(), test.clone(), None))
+                }
+                ("filter",
+                 [Expression::Symbol(tag_or_name), Expression::String(test), Expression::String(setup)]) => {
+                    if is_pipelined {
+                        Ok(Application::ScriptedFilterPipedNamed(tag_or_name.clone(), test.clone(), Some(setup.clone())))
+                    } else {
+                        Ok(Application::ScriptedFilter(tag_or_name.clone(), test.clone(), Some(setup.clone())))
+                    }
+                }
+                ("filter",
+                 [Expression::Symbol(tag), Expression::Symbol(name), Expression::String(test), Expression::String(setup)]) => {
+                    Ok(Application::ScriptedFilterNamed(tag.clone(), name.clone(), test.clone(), Some(setup.clone())))
+                }
+                ("filter", [Expression::String(test)]) => {
+                    Ok(Application::ScriptedFilterPiped(test.clone(), None))
+                }
+                ("filter", [Expression::String(test), Expression::String(setup)]) => {
+                    Ok(Application::ScriptedFilterPiped(test.clone(), Some(setup.clone())))
+                }
 
                 ("take", [Expression::Symbol(log), Expression::Int(count)]) => {
                     Ok(Application::Take(log.clone(), *count))
                 }
-                ("take", [Expression::Int(count)]) => Ok(Application::TakePipe(*count)),
-
-                (
-                    "transform",
-                    [Expression::Symbol(tag), Expression::String(transform), Expression::String(setup)],
-                ) => Ok(Application::Transform(
-                    tag.clone(),
-                    transform.clone(),
-                    Some(setup.clone()),
-                )),
-                ("transform", [Expression::Symbol(tag), Expression::String(transform)]) => {
-                    Ok(Application::Transform(tag.clone(), transform.clone(), None))
-                }
-                ("transform", [Expression::String(transform), Expression::String(setup)]) => Ok(
-                    Application::TransformPipe(transform.clone(), Some(setup.clone())),
-                ),
-                ("transform", [Expression::String(transform)]) => {
-                    Ok(Application::TransformPipe(transform.clone(), None))
+                ("take", [Expression::Int(count)]) => {
+                    Ok(Application::TakePipe(*count))
                 }
 
                 _ => Err(SyntaxError::UnknownFunction),
@@ -76,15 +140,23 @@ impl Application {
     fn is_pipelined(&self) -> bool {
         match self {
             Application::Load(_, _) => false,
-            Application::Regex(_, _) => false,
             Application::Tag(_, _) => false,
-            Application::Take(_, _) => false,
+            Application::Regex(_, _) => false,
             Application::Transform(_, _, _) => false,
+            Application::DirectFilter(_, _, _) => false,
+            Application::DirectFilterNamed(_, _, _, _) => false,
+            Application::ScriptedFilter(_, _, _) => false,
+            Application::ScriptedFilterNamed(_, _, _, _) => false,
+            Application::Take(_, _) => false,
 
-            Application::RegexPipe(_) => true,
-            Application::TagPipe(_) => true,
+            Application::TagPiped(_) => true,
+            Application::RegexPiped(_) => true,
+            Application::TransformPiped(_, _) => true,
+            Application::DirectFilterPiped(_, _) => true,
+            Application::DirectFilterPipedNamed(_, _, _) => true,
+            Application::ScriptedFilterPiped(_, _) => true,
+            Application::ScriptedFilterPipedNamed(_, _, _) => true,
             Application::TakePipe(_) => true,
-            Application::TransformPipe(_, _) => true,
         }
     }
 }
@@ -112,20 +184,7 @@ impl Repl {
                 *self.symbols.entry(name.to_string()).or_insert(output.id) = output.id;
                 Ok(output)
             }
-            Application::Regex(tag_name, regex) => {
-                if let Some(Id::Tag(tag_id)) = self.symbols.get(&tag_name) {
-                    engine.run_command(&Command::Regex(*tag_id, regex))
-                } else {
-                    Err(Error::TagDoesNotExist(tag_name))
-                }
-            }
-            Application::RegexPipe(regex) => {
-                if let Some(Id::Tag(tag_id)) = target {
-                    engine.run_command(&Command::Regex(tag_id, regex))
-                } else {
-                    Err(Error::InvalidTarget(format!("{:?}", target)))
-                }
-            }
+
             Application::Tag(file_name, tag_name) => {
                 if let Some(Id::File(file_id)) = self.symbols.get(&file_name) {
                     let output = engine.run_command(&Command::Tag(*file_id, tag_name.clone()))?;
@@ -135,7 +194,7 @@ impl Repl {
                     Err(Error::FileNotLoaded(file_name))
                 }
             }
-            Application::TagPipe(tag_name) => {
+            Application::TagPiped(tag_name) => {
                 if let Some(Id::File(file_id)) = target {
                     let output =
                         engine.run_command(&Command::Tag(file_id, tag_name.to_string()))?;
@@ -148,30 +207,117 @@ impl Repl {
                     Err(Error::InvalidTarget(format!("{:?}", target)))
                 }
             }
-            Application::Take(file_name, count) => {
-                if let Some(Id::File(file_id)) = self.symbols.get(&file_name) {
-                    engine.run_command(&Command::Take(*file_id, count))
+
+            Application::Regex(tag_name, regex) => {
+                if let Some(Id::Tag(tag_id)) = self.symbols.get(&tag_name) {
+                    engine.run_command(&Command::Regex(*tag_id, regex))
                 } else {
-                    Err(Error::FileNotLoaded(file_name))
+                    Err(Error::SymbolNotFound(tag_name))
                 }
             }
-            Application::TakePipe(count) => {
-                if let Some(Id::File(file_id)) = target {
-                    engine.run_command(&Command::Take(file_id, count))
+            Application::RegexPiped(regex) => {
+                if let Some(Id::Tag(tag_id)) = target {
+                    engine.run_command(&Command::Regex(tag_id, regex))
                 } else {
                     Err(Error::InvalidTarget(format!("{:?}", target)))
                 }
             }
+
             Application::Transform(tag_name, transform, setup) => {
                 if let Some(Id::Tag(tag_id)) = self.symbols.get(&tag_name) {
                     engine.run_command(&Command::Transform(*tag_id, transform, setup))
                 } else {
-                    Err(Error::TagDoesNotExist(tag_name))
+                    Err(Error::SymbolNotFound(tag_name))
                 }
             }
-            Application::TransformPipe(transform, setup) => {
+            Application::TransformPiped(transform, setup) => {
                 if let Some(Id::Tag(tag_id)) = target {
                     engine.run_command(&Command::Transform(tag_id, transform, setup))
+                } else {
+                    Err(Error::InvalidTarget(format!("{:?}", target)))
+                }
+            }
+
+            Application::DirectFilter(tag_name, comparator, value) => {
+                if let Some(Id::Tag(tag_id)) = self.symbols.get(&tag_name) {
+                    engine.run_command(&Command::DirectFilter(*tag_id, comparator, value))
+                } else {
+                    Err(Error::SymbolNotFound(tag_name))
+                }
+            }
+            Application::DirectFilterNamed(tag_name, filter_name, comparator, value) => {
+                if let Some(Id::Tag(tag_id)) = self.symbols.get(&tag_name) {
+                    let output =
+                        engine.run_command(&Command::DirectFilter(*tag_id, comparator, value))?;
+                    *self.symbols.entry(filter_name).or_insert(output.id) = output.id;
+                    Ok(output)
+                } else {
+                    Err(Error::SymbolNotFound(tag_name))
+                }
+            }
+            Application::DirectFilterPiped(comparator, value) => {
+                if let Some(Id::Tag(tag_id)) = target {
+                    engine.run_command(&Command::DirectFilter(tag_id, comparator, value))
+                } else {
+                    Err(Error::InvalidTarget(format!("{:?}", target)))
+                }
+            }
+            Application::DirectFilterPipedNamed(filter_name, comparator, value) => {
+                if let Some(Id::Tag(tag_id)) = target {
+                    let output =
+                        engine.run_command(&Command::DirectFilter(tag_id, comparator, value))?;
+                    *self.symbols.entry(filter_name).or_insert(output.id) = output.id;
+                    Ok(output)
+                } else {
+                    Err(Error::InvalidTarget(format!("{:?}", target)))
+                }
+            }
+
+            Application::ScriptedFilter(tag_name, test, setup) => {
+                if let Some(Id::Tag(tag_id)) = self.symbols.get(&tag_name) {
+                    engine.run_command(&Command::ScriptedFilter(*tag_id, test, setup))
+                } else {
+                    Err(Error::SymbolNotFound(tag_name))
+                }
+            }
+            Application::ScriptedFilterNamed(tag_name, filter_name, test, setup) => {
+                if let Some(Id::Tag(tag_id)) = self.symbols.get(&tag_name) {
+                    let output =
+                        engine.run_command(&Command::ScriptedFilter(*tag_id, test, setup))?;
+                    *self.symbols.entry(filter_name).or_insert(output.id) = output.id;
+                    Ok(output)
+                } else {
+                    Err(Error::SymbolNotFound(tag_name))
+                }
+            }
+            Application::ScriptedFilterPiped(test, setup) => {
+                if let Some(Id::Tag(tag_id)) = target {
+                    engine.run_command(&Command::ScriptedFilter(tag_id, test, setup))
+                } else {
+                    Err(Error::InvalidTarget(format!("{:?}", target)))
+                }
+            }
+            Application::ScriptedFilterPipedNamed(filter_name, test, setup) => {
+                if let Some(Id::Tag(tag_id)) = target {
+                    let output =
+                        engine.run_command(&Command::ScriptedFilter(tag_id, test, setup))?;
+                    *self.symbols.entry(filter_name).or_insert(output.id) = output.id;
+                    Ok(output)
+                } else {
+                    Err(Error::InvalidTarget(format!("{:?}", target)))
+                }
+            }
+
+            Application::Take(name, count) => {
+                if let Some(id) = self.symbols.get(&name) {
+                    engine.run_command(&Command::Take(*id, count))
+                } else {
+                    Err(Error::SymbolNotFound(name))
+                }
+            }
+            Application::TakePipe(count) => {
+                if let Some(Id::File(file_id)) = target {
+                    engine.run_command(&Command::Take(Id::File(file_id), count))
                 } else {
                     Err(Error::InvalidTarget(format!("{:?}", target)))
                 }
@@ -187,13 +333,13 @@ enum ParseState {
     Pipelined(Application),
 }
 
-fn parse_line(line: &str, is_continuation: bool) -> Result<ParseState> {
-    if !is_continuation && line == "" {
+fn parse_line(line: &str, is_pipelined: bool) -> Result<ParseState> {
+    if !is_pipelined && line == "" {
         return Ok(ParseState::Empty);
     }
 
     match parser::parse_expression(&line) {
-        Ok((_, exp)) => match Application::from_expression(&exp) {
+        Ok((_, exp)) => match Application::from_expression(&exp, is_pipelined) {
             Ok(func) if func.is_pipelined() => Ok(ParseState::Pipelined(func)),
             Ok(func) => Ok(ParseState::Root(func)),
             Err(err) => Err(Error::Syntax(err, line.to_string())),
